4dc9c3429ffe35208e191fdadf33c0f3e249ddcf
Roza Bruggeman <rosebridge666@gmail.com>
2024-11-07 11:08:46 +0100

    aboutus

--- php/aboutus.php
+++ php/aboutus.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - GlowFlow</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+<?php include 'navbar.php'; ?>   
     <div class="container">
         <!-- Menu Icon -->
         <div class="menu-icon">
