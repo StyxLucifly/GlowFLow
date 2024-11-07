1458dfa65efe78054ec2ba4f4e0891a6d68f6332
Roza Bruggeman <rosebridge666@gmail.com>
2024-11-07 11:17:17 +0100

    aboutus

--- php/aboutus.php
+++ php/aboutus.php
@@ -48,9 +48,10 @@ Voordat ik mijn studie HBO-ICT begon, heb ik MBO-niveau 4 Software Developer ged
                 <div class="team-member">
                     <div class="profile-pic"></div>
                     <h3>Daan</h3>
-                    <p>Mijn naam is Remco Toonders, ik ben 19 jaar oud en woon in Someren samen met mijn ouders, mijn zus en onze hond, in mijn vrije tijd ben ik graag bezig met mijn hobby's zoals Formule 1, uitgaan en alles wat met auto's te maken heeft!
-Voordat ik mijn studie HBO-ICT begon, heb ik MBO-niveau 4 Software Developer gedaan op het ROC Ter Aa in Helmond. Toen ik klaar was met mijn Mbo-opleiding, was ik pas 18 jaar. dat vond ik nog te jong om meteen te gaan werken en merkte dat ik daar nog niet aan toe was. Daarom koos ik ervoor om verder te studeren op het Fontys.
- Naast mijn studie werk ik al ruim 3 jaar op de zaterdagen bij een kippenboer in Someren-Heide, waar ik help met verschillende taken in en rond de stallen. </p>
+                    <p>Mijn naam is Daan Strijbos, ik ben 22 jaar oud en woon in Eindhoven samen met mijn ouders, mijn broer en zus en onze hond. In mijn vrije tijd ben ik graag bezig met mijn hobby's zoals voetbal, grafisch ontwerpen en gaming!
+Voordat ik mijn studie HBO-ICT begon, heb ik VWO-Gymnasium afgerond op het Huygens Lyceum in Eindhoven. Toen ik klaar was met mijn VWO-opleiding, heb ik een aantal jaar aan zelfstudie gedaan en heb ik meerdere baantjes gehad om werkervaring op te doen. Uiteindelijk vond ik het wel tijd om nog een diploma te halen.
+Naast mijn studie doe ik af en toe klusjes op het gebied van grafisch ontwerp voor mkb’s en evenementen. Ook werk ik bij de front-office van een fysiotherapeutenpraktijk.
+ </p>
                     <div class="social-icons">
                         <a href="#"><img src="instagram-icon.png" alt="Instagram"></a>
                         <a href="#"><img src="x-icon.png" alt="X (Twitter)"></a>
@@ -92,9 +93,12 @@ Voordat ik mijn studie HBO-ICT begon, heb ik MBO-niveau 4 Software Developer ged
                 <div class="team-member">
                     <div class="profile-pic"></div>
                     <h3>Roza Bruggeman</h3>
-                    <p>Mijn naam is Roza Bruggeman, ik ben 24 jaar oud en woon nu in Eindhoven met de vriendin van mijn vader. In mijn vrije tijd spendeer ik mijn tijd graag met vrienden/familie en anders ben ik bezig met mijn hobby's. 
+                    <p>Mijn naam is Roza Bruggeman, ik ben 24 jaar oud en woon nu in Eindhoven met de vriendin van mijn vader. In mijn vrije tijd spendeer ik mijn tijd graag met vrienden/familie en anders ben ik bezig met mijn hobby's zoals tekenen, schilderen, koken, gitaar spelen en gamen. 
+
 Voordat ik bij de hbo-ICT opleiding ben begonnen heb ik mbo-niveau 3 zelfstandig werkend kok gevolgd op de Rooi pannen te Tilburg. Na een paar jaar in de horeca gewerkt te hebben was ik toe aan een andere richting. 
+
 Ik ben al lang gepassioneerd over de staat waar de aarde zich in verkeerd. Het klimaat heeft een onomkeerbaar punt al gepasseerd, maar dat betekent niet dat we minder moeten doen. Juist nu is nog de tijd om een verandering te kunnen maken, daarom draag ik graag bij met ons idee: GlowFlow. 
+ 
 </p>
                     <div class="social-icons">
                         <a href="#"><img src="instagram-icon.png" alt="Instagram"></a>
