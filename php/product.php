a2e2d9c93d40b981876e74160dc9f752e7c22f19
Roza Bruggeman <rosebridge666@gmail.com>
2024-11-08 09:26:52 +0100

    test git

--- php/product.php
+++ php/product.php
@@ -6,6 +6,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- test -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../img/BulbIcon.png">
